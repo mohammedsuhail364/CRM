4bc764897fe04dfa68f3bc61f7cdfcb6df0b4b24
mohammedsuhail364 <mohammedthanish364@gmail.com>
2025-12-20 20:56:46 +0530

    include the path

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const path=require('path');
 
 const customerRoutes = require("./routes/customerRoutes");
 const ticketRoutes = require("./routes/ticketRoutes");
